aad1c54bce2e7c5b5adb78be25c36b6b814936b6
sdh0572 <sdh0572@gmail.com>
2024-07-17 22:24:01 +0900

    2.3 Firebase Project

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Login from "./routes/login";
 import CreateAccount from "./routes/create-account";
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import { useEffect, useState } from "react";
+import LoadingScreen from "./component/loading-screen";
 
 const router = createBrowserRouter([
   {
@@ -44,11 +46,20 @@ const GlobalStyles = createGlobalStyle`
 `;
 
 function App() {
-  return <>
+  const  [isLoading, setLoading] = useState(true);
+  const init=async() => {
+    //wait for firebase
+    setLoading(false);
+  };
+  useEffect(() => {
+    init();
+  }, []);
+  return (
+  <>
     <GlobalStyles />
-    <RouterProvider router = {router} />
-  </>;
-  
+    {isLoading ? <LoadingScreen /> : <RouterProvider router={router}/>}
+  </>
+  );
 }
 
 export default App
---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkj3PGaquo6Oy-D8OogbD_NeVBx-t97HE",
+  authDomain: "nwitter-reloaded-14fbd.firebaseapp.com",
+  projectId: "nwitter-reloaded-14fbd",
+  storageBucket: "nwitter-reloaded-14fbd.appspot.com",
+  messagingSenderId: "177569340058",
+  appId: "1:177569340058:web:96b0be1059772370af9d23"
+};
+
+const app = initializeApp(firebaseConfig);
